07f93d550530a7ffc2375b76e4ce1ae2933eaa80
Daniel Schmidt <info@daniel-schmidt.me>
2023-05-15 17:56:33 +0200

    Fix syscall on windows.

--- kill_windows.go
+++ kill_windows.go
@@ -5,5 +5,16 @@ import (
 )
 
 func SysKill() {
-	syscall.GenerateConsoleCtrlEvent(syscall.CTRL_C_EVENT, 0)
+	d, err := syscall.LoadDLL("kernel32.dll")
+	if err != nil {
+		return
+	}
+	p, err := d.FindProc("GenerateConsoleCtrlEvent")
+	if err != nil {
+		return
+	}
+	r, _, err := p.Call(syscall.CTRL_BREAK_EVENT, uintptr(syscall.Getpid()))
+	if r == 0 {
+		return
+	}
 }
