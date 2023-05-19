d3252b9dd6f066c7ec9e862855f5d67e05780214
Daniel Schmidt <info@daniel-schmidt.me>
2023-05-19 19:57:38 +0200

    Better key handling.

--- bubbletea/adapter_bubbletea.go
+++ bubbletea/adapter_bubbletea.go
@@ -5,50 +5,88 @@ import (
 	tea "github.com/charmbracelet/bubbletea"
 	"github.com/hajimehoshi/ebiten/v2"
 	"github.com/hajimehoshi/ebiten/v2/inpututil"
-	"strings"
+	"unicode"
 )
 
-var ebitenToTeaKeys = map[ebiten.Key]tea.KeyType{
-	ebiten.KeyEnter:      tea.KeyEnter,
-	ebiten.KeyTab:        tea.KeyTab,
-	ebiten.KeySpace:      tea.KeySpace,
-	ebiten.KeyBackspace:  tea.KeyBackspace,
-	ebiten.KeyDelete:     tea.KeyDelete,
-	ebiten.KeyHome:       tea.KeyHome,
-	ebiten.KeyEnd:        tea.KeyEnd,
-	ebiten.KeyPageUp:     tea.KeyPgUp,
-	ebiten.KeyArrowUp:    tea.KeyUp,
-	ebiten.KeyArrowDown:  tea.KeyDown,
-	ebiten.KeyArrowLeft:  tea.KeyLeft,
-	ebiten.KeyArrowRight: tea.KeyRight,
-	ebiten.KeyEscape:     tea.KeyEscape,
+type teaKey struct {
+	key  tea.KeyType
+	rune []rune
 }
 
-var ebitenToTeaRunes = map[ebiten.Key][]rune{
-	ebiten.Key1:            {'1'},
-	ebiten.Key2:            {'2'},
-	ebiten.Key3:            {'3'},
-	ebiten.Key4:            {'4'},
-	ebiten.Key5:            {'5'},
-	ebiten.Key6:            {'6'},
-	ebiten.Key7:            {'7'},
-	ebiten.Key8:            {'8'},
-	ebiten.Key9:            {'9'},
-	ebiten.Key0:            {'0'},
-	ebiten.KeyEnter:        {'\n'},
-	ebiten.KeyTab:          {'\t'},
-	ebiten.KeySpace:        {' '},
-	ebiten.KeyPeriod:       {'.'},
-	ebiten.KeySlash:        {'/'},
-	ebiten.KeyBackslash:    {'\\'},
-	ebiten.KeyMinus:        {'-'},
-	ebiten.KeyEqual:        {'='},
-	ebiten.KeySemicolon:    {';'},
-	ebiten.KeyApostrophe:   {'\''},
-	ebiten.KeyGraveAccent:  {'`'},
-	ebiten.KeyComma:        {','},
-	ebiten.KeyLeftBracket:  {'['},
-	ebiten.KeyRightBracket: {']'},
+var ebitenToTeaKeys = map[ebiten.Key]teaKey{
+	ebiten.KeyEnter:        {tea.KeyEnter, []rune{'\n'}},
+	ebiten.KeyTab:          {tea.KeyTab, []rune{'\t'}},
+	ebiten.KeySpace:        {tea.KeySpace, []rune{' '}},
+	ebiten.KeyBackspace:    {tea.KeyBackspace, []rune{}},
+	ebiten.KeyDelete:       {tea.KeyDelete, []rune{}},
+	ebiten.KeyHome:         {tea.KeyHome, []rune{}},
+	ebiten.KeyEnd:          {tea.KeyEnd, []rune{}},
+	ebiten.KeyPageUp:       {tea.KeyPgUp, []rune{}},
+	ebiten.KeyArrowUp:      {tea.KeyUp, []rune{}},
+	ebiten.KeyArrowDown:    {tea.KeyDown, []rune{}},
+	ebiten.KeyArrowLeft:    {tea.KeyLeft, []rune{}},
+	ebiten.KeyArrowRight:   {tea.KeyRight, []rune{}},
+	ebiten.KeyEscape:       {tea.KeyEscape, []rune{}},
+	ebiten.Key1:            {tea.KeyRunes, []rune{'1'}},
+	ebiten.Key2:            {tea.KeyRunes, []rune{'2'}},
+	ebiten.Key3:            {tea.KeyRunes, []rune{'3'}},
+	ebiten.Key4:            {tea.KeyRunes, []rune{'4'}},
+	ebiten.Key5:            {tea.KeyRunes, []rune{'5'}},
+	ebiten.Key6:            {tea.KeyRunes, []rune{'6'}},
+	ebiten.Key7:            {tea.KeyRunes, []rune{'7'}},
+	ebiten.Key8:            {tea.KeyRunes, []rune{'8'}},
+	ebiten.Key9:            {tea.KeyRunes, []rune{'9'}},
+	ebiten.Key0:            {tea.KeyRunes, []rune{'0'}},
+	ebiten.KeyA:            {tea.KeyRunes, []rune{'a'}},
+	ebiten.KeyB:            {tea.KeyRunes, []rune{'b'}},
+	ebiten.KeyC:            {tea.KeyRunes, []rune{'c'}},
+	ebiten.KeyD:            {tea.KeyRunes, []rune{'d'}},
+	ebiten.KeyE:            {tea.KeyRunes, []rune{'e'}},
+	ebiten.KeyF:            {tea.KeyRunes, []rune{'f'}},
+	ebiten.KeyG:            {tea.KeyRunes, []rune{'g'}},
+	ebiten.KeyH:            {tea.KeyRunes, []rune{'h'}},
+	ebiten.KeyI:            {tea.KeyRunes, []rune{'i'}},
+	ebiten.KeyJ:            {tea.KeyRunes, []rune{'j'}},
+	ebiten.KeyK:            {tea.KeyRunes, []rune{'k'}},
+	ebiten.KeyL:            {tea.KeyRunes, []rune{'l'}},
+	ebiten.KeyM:            {tea.KeyRunes, []rune{'m'}},
+	ebiten.KeyN:            {tea.KeyRunes, []rune{'n'}},
+	ebiten.KeyO:            {tea.KeyRunes, []rune{'o'}},
+	ebiten.KeyP:            {tea.KeyRunes, []rune{'p'}},
+	ebiten.KeyQ:            {tea.KeyRunes, []rune{'q'}},
+	ebiten.KeyR:            {tea.KeyRunes, []rune{'r'}},
+	ebiten.KeyS:            {tea.KeyRunes, []rune{'s'}},
+	ebiten.KeyT:            {tea.KeyRunes, []rune{'t'}},
+	ebiten.KeyU:            {tea.KeyRunes, []rune{'u'}},
+	ebiten.KeyV:            {tea.KeyRunes, []rune{'v'}},
+	ebiten.KeyW:            {tea.KeyRunes, []rune{'w'}},
+	ebiten.KeyX:            {tea.KeyRunes, []rune{'x'}},
+	ebiten.KeyY:            {tea.KeyRunes, []rune{'y'}},
+	ebiten.KeyZ:            {tea.KeyRunes, []rune{'z'}},
+	ebiten.KeyComma:        {tea.KeyRunes, []rune{','}},
+	ebiten.KeyPeriod:       {tea.KeyRunes, []rune{'.'}},
+	ebiten.KeySlash:        {tea.KeyRunes, []rune{'/'}},
+	ebiten.KeyBackslash:    {tea.KeyRunes, []rune{'\\'}},
+	ebiten.KeySemicolon:    {tea.KeyRunes, []rune{';'}},
+	ebiten.KeyApostrophe:   {tea.KeyRunes, []rune{'\''}},
+	ebiten.KeyGraveAccent:  {tea.KeyRunes, []rune{'`'}},
+	ebiten.KeyEqual:        {tea.KeyRunes, []rune{'='}},
+	ebiten.KeyMinus:        {tea.KeyRunes, []rune{'-'}},
+	ebiten.KeyLeftBracket:  {tea.KeyRunes, []rune{'['}},
+	ebiten.KeyRightBracket: {tea.KeyRunes, []rune{']'}},
+	ebiten.KeyF1:           {tea.KeyF1, []rune{}},
+	ebiten.KeyF2:           {tea.KeyF2, []rune{}},
+	ebiten.KeyF3:           {tea.KeyF3, []rune{}},
+	ebiten.KeyF4:           {tea.KeyF4, []rune{}},
+	ebiten.KeyF5:           {tea.KeyF5, []rune{}},
+	ebiten.KeyF6:           {tea.KeyF6, []rune{}},
+	ebiten.KeyF7:           {tea.KeyF7, []rune{}},
+	ebiten.KeyF8:           {tea.KeyF8, []rune{}},
+	ebiten.KeyF9:           {tea.KeyF9, []rune{}},
+	ebiten.KeyF10:          {tea.KeyF10, []rune{}},
+	ebiten.KeyF11:          {tea.KeyF11, []rune{}},
+	ebiten.KeyF12:          {tea.KeyF12, []rune{}},
+	ebiten.KeyShift:        {tea.KeyShiftLeft, []rune{}},
 }
 
 var ebitenToTeaMouse = map[ebiten.MouseButton]tea.MouseEventType{
@@ -134,21 +172,30 @@ func (b *Adapter) HandleKeyPress() {
 	keys = inpututil.AppendJustReleasedKeys(keys)
 
 	for _, k := range keys {
-		if val, ok := ebitenToTeaKeys[k]; ok {
-			runes := []rune(strings.ToLower(k.String()))
-			b.prog.Send(tea.KeyMsg{
-				Type:  val,
-				Runes: runes,
-				Alt:   ebiten.IsKeyPressed(ebiten.KeyAlt),
-			})
-		} else {
-			var runes []rune
-			if val, ok := ebitenToTeaRunes[k]; ok {
-				runes = val
+
+		switch k {
+		case ebiten.KeyC:
+			if ebiten.IsKeyPressed(ebiten.KeyControl) {
+				b.prog.Send(tea.KeyMsg{
+					Type:  tea.KeyCtrlC,
+					Runes: []rune{},
+					Alt:   false,
+				})
+
+				continue
 			}
+		}
+
+		if val, ok := ebitenToTeaKeys[k]; ok {
+			if ebiten.IsKeyPressed(ebiten.KeyShift) {
+				for i := range val.rune {
+					val.rune[i] = unicode.ToUpper(val.rune[i])
+				}
+			}
+
 			b.prog.Send(tea.KeyMsg{
-				Type:  tea.KeyRunes,
-				Runes: runes,
+				Type:  val.key,
+				Runes: val.rune,
 				Alt:   ebiten.IsKeyPressed(ebiten.KeyAlt),
 			})
 		}
