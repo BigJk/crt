87fafa02e6a49e883d733755f0c8400e49b321c6
Daniel Schmidt <info@daniel-schmidt.me>
2023-05-19 19:14:46 +0200

    Fix exit on windows.

--- kill_windows.go
+++ kill_windows.go
@@ -1,20 +1,9 @@
 package crt
 
 import (
-	"syscall"
+	"os"
 )
 
 func SysKill() {
-	d, err := syscall.LoadDLL("kernel32.dll")
-	if err != nil {
-		return
-	}
-	p, err := d.FindProc("GenerateConsoleCtrlEvent")
-	if err != nil {
-		return
-	}
-	r, _, err := p.Call(syscall.CTRL_BREAK_EVENT, uintptr(syscall.Getpid()))
-	if r == 0 {
-		return
-	}
+	os.Exit(1)
 }
